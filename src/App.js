2acf5002bb3143661bd99d861793ac4d3e8fca56
Achieve-software <vtcbcdtyr@gmail.com>
2023-07-03 21:38:05 +0300

    router-example

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Path from "./pages/Path";
+import Home from "./pages/Home";
+import People from "./pages/People";
+import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/people" element={<People/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/Path" element={<Path/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
